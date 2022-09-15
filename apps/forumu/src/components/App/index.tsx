99ed7af89fb41dceaafb6076ad81aca57a9f3f77
Marco A. Miranda S <marco2015.sandoval@gmail.com>
2022-09-14 22:18:31 -0300

    :lipstick: Update UI and improve test

--- apps/forumu/src/components/App/index.tsx
+++ apps/forumu/src/components/App/index.tsx
@@ -1,6 +1,6 @@
-import { Input } from "@/components/Input";
 import { useForumu } from "@sutiles/forumu";
 import { useState } from "react";
+import { Input } from "../Input";
 
 enum Page {
   form = "form",
@@ -11,16 +11,39 @@ const initValues = {
   username: "",
   email: "",
   password: "",
+  gender: "male",
   agree: false,
   country: "",
 };
 
 export const App = () => {
   const [page, setPage] = useState<Page>(Page.form);
-  const { fields, errors, props } = useForumu({
+  const { fields, touched, errors, props } = useForumu({
     initValues,
     onSubmit: () => setPage(Page.completed),
-    validator: ({ agree }, errors) => {
+    validator: ({ agree, username, password, email }, errors) => {
+      // Username validator
+      if (!username) {
+        errors.username = "Username is required";
+      } else if (username.length <= 4) {
+        errors.username = "Username too short";
+      } else if (username.length >= 16) {
+        errors.username = "Username too long";
+      }
+
+      // Email validator
+      if (!email) {
+        errors.email = "Email is required";
+      } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+        errors.email = "Invalid email";
+      }
+
+      // Password validator
+      if (!password) {
+        errors.password = "Password is required";
+      } else if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)) {
+        errors.password = "Invalid password";
+      }
       !agree && (errors.agree = "You need to accept before continue");
     },
   });
@@ -32,22 +55,47 @@ export const App = () => {
           <h1 className="text-2xl text-center">Forumu Example</h1>
           <p className="mt-1 mb-16 text-center">Test of library</p>
           <form className="flex flex-col gap-4" {...props.form}>
-            <Input label="Partner" name="partner" value="Seudosan" readOnly />
-            <Input label="Username" name="username" autoComplete="username" value={fields.username} {...props.input} />
-            <Input label="Email" name="email" autoComplete="email" value={fields.email} {...props.input} />
+            <Input label="Partner" name="partner" value="Seudosan" readOnly tabIndex={-1} />
             <Input
+              isDangerous={!!(errors.username && touched.username)}
+              label="Username"
+              name="username"
+              autoComplete="username"
+              helper={touched.username && errors.username}
+              value={fields.username}
+              {...props.input}
+            />
+            <Input
+              isDangerous={!!(errors.email && touched.email)}
+              label="Email"
+              name="email"
+              autoComplete="email"
+              helper={touched.email && errors.email}
+              value={fields.email}
+              {...props.input}
+            />
+            <Input
+              isDangerous={!!(errors.password && touched.password)}
               label="Password"
               name="password"
               type="password"
               autoComplete="new-password"
-              helper="Secure password:"
+              helper={touched.password && errors.password}
               value={fields.password}
               {...props.input}
             />
             <label htmlFor="agree">
-              <input type="checkbox" name="agree" id="agree" checked={fields.agree} {...props.input} /> Estoy de acuerdo
+              <input type="checkbox" name="agree" id="agree" checked={fields.agree} {...props.input} /> I'm agree
               <span className="mt-1 block text-red-500">{errors.agree}</span>
             </label>
+            <div className="flex space-x-4">
+              <label>
+                <input type="radio" name="gender" value="male" checked={fields.gender === "male"} {...props.input} /> Male
+              </label>
+              <label>
+                <input type="radio" name="gender" value="female" checked={fields.gender === "female"} {...props.input} /> Female
+              </label>
+            </div>
             <button type="submit" className="mt-4 bg-violet-600 p-2.5 text-slate-50 font-medium rounded-md">
               Submit
             </button>
@@ -63,6 +111,9 @@ export const App = () => {
           <div className="mt-4">
             <p className="flex justify-between gap-3">
               <span>Username:</span> <span>{fields.username}</span>
+            </p>
+            <p className="flex justify-between gap-3">
+              <span>Genre:</span> <span>{fields.gender}</span>
             </p>
             <p className="flex justify-between gap-3">
               <span>Email:</span> <span>{fields.email}</span>
--- apps/forumu/src/components/Input/index.tsx
+++ apps/forumu/src/components/Input/index.tsx
@@ -1,18 +1,36 @@
-import { FunctionComponent, InputHTMLAttributes, ReactNode } from "react";
+import clsx from "clsx";
+import { InputHTMLAttributes, ReactNode } from "react";
 
-interface Input extends InputHTMLAttributes<HTMLInputElement> {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label: ReactNode;
   helper?: ReactNode;
-  error?: boolean;
   name: string;
+  isDangerous?: boolean;
 }
 
-export const Input: FunctionComponent<Input> = ({ label, name, helper, error, ...restProps }) => {
+const INPUT_CLASSES = {
+  normal: "border-slate-600",
+  danger: "border-red-600",
+};
+
+const HELPER_CLASSES = {
+  normal: 'text-slate-600"',
+  danger: "text-red-600",
+};
+
+export const Input = ({ label, name, helper, isDangerous, ...restProps }: Props) => {
+  const _labelClasses = clsx("flex flex-col text-slate-900");
+  const _inputClasses = clsx(
+    "mt-1 px-3 py-2 bg-slate-50 border  rounded-md focus:outline-violet-600 read-only:outline-0",
+    isDangerous ? INPUT_CLASSES.danger : INPUT_CLASSES.normal,
+  );
+  const _helperClasses = clsx(isDangerous ? HELPER_CLASSES.danger : HELPER_CLASSES.normal);
+
   return (
-    <label htmlFor={name} className="flex flex-col text-slate-900">
+    <label htmlFor={name} className={_labelClasses}>
       {label}
-      <input name={name} id={name} type="text" className="mt-1 px-3 py-2 bg-slate-50 border border-slate-600 rounded-md" {...restProps} />
-      {helper && <p className="text-slate-600">{helper}</p>}
+      <input name={name} id={name} type="text" className={_inputClasses} {...restProps} />
+      {helper && <p className={_helperClasses}>{helper}</p>}
     </label>
   );
 };
